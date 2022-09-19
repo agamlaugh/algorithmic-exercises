f266ec1401cca5352e55af27d9befac8cab63448
Agam Cohen <agamlaugh@gmail.com>
2022-09-19 15:02:06 -0700

    I started with a plan. I used a couple of previous exercises for reference. Then I fixed all the bugs by using the debugging feature. The first bug was that I had to reset my counter and min value. I learned this from exercise 6. I was still getting an error. I had to use try except to fix it. I'm very happy with how this turned out

---
+++ algorithmsEx7.py
@@ -0,0 +1,23 @@
+list1 = [3,6,9,3,5,1,0]
+list2 = [4,-1,-5,7,2,8,-9]
+list3 = list1 + list2
+list4 = []
+counter = 0
+min = list3[counter]
+
+while len(list3)>0:
+    while counter<len(list3):
+        if min>list3[counter]:
+            min = list3[counter]
+        counter = counter + 1
+    list4.append(min)
+    list3.remove(min)
+    counter = 0
+    try:
+        min = list3[counter]
+    except:
+        pass
+
+
+print(list4)
+
