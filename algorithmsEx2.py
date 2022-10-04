3b327496a9f96386ce8b0026ac17e916c6fa7049
Agam Cohen <agamlaugh@gmail.com>
2022-10-04 00:45:30 -0700

    quick wording fix

--- algorithmsEx2.py
+++ algorithmsEx2.py
@@ -1,6 +1,6 @@
 # create a loop that checks all number between 1 and 299
 for number in range(1, 299):
-    # store a variable to ensure that the values are integers. this way we can manipulate them as numbers(through operators and modifiers) to get a desired result
+    # store a variable to make the number a string. this way the number can be searched for like a character
     numberStr = str(number)
     # check if number has a 3 in it. if so then print the number
     if numberStr.__contains__('3'):
