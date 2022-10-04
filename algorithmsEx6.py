761419c6f663a91f30eb3844b8bb5c57b78f0de3
Agam Cohen <agamlaugh@gmail.com>
2022-10-04 00:18:27 -0700

    I fixed all of your notes and I documented the code process

--- algorithmsEx6.py
+++ algorithmsEx6.py
@@ -1,44 +1,58 @@
-# print('Enter 7 values for a list:')
-# numList = [input(), input(), input(), input(), input(), input(), input()]
-# newList = []
-# counter = 1 
-
-# for counter in  range(1, 6):
-#     if numList[counter] == newList[counter-1]:
-#         print(numList[counter], "is a duplicate number")
-#     else:
-#         newList.append(numList[counter])
-        
-# print(newList)
-
-# #doesn't work
-
 from re import S
 
 
-print('Enter 7 values for a list:') # I want to add 12 values today and 16 tomorrow. Find a way for the user to choose how many items he wants to insert into the list
-numList = [input(), input(), input(), input(), input(), input(), input()] # There is a better way than this, please see previous comment
+print('Enter how many values you want in your list:') 
+inputAmount = int(input())
+numList = [] 
+print(f"Enter {inputAmount} values:")
+#create a for loop to input values as much as the user previously instructed
+for numValue in range(inputAmount):
+    numList.append(input())
 newList = []
-coinfound = False # what is this variable name? Are we dealing with coins here? Please use indicative variable names.
-counter = 0
+# store a variable to know when there a duplicate number is found
+duplicateFound = False 
 
+#begin by adding the first value of numList to the empty list. without a value in the newlist the future for loops will not run. 
 newList.append(numList[0])
-numList.pop(0)
 
-while len(numList) > 0:
-    while coinfound == False and counter < len(newList): #please replace while with for loop and insert coinfound condition inside loop in an if statement
-        if numList[0] == newList[counter]:
-            coinfound = True
-        else:
-            counter = counter + 1
-    counter = 0
-    if coinfound == False:
-        newList.append(numList[0])
-        numList.pop(0)
-    else:
-        numList.pop(0)
-        coinfound = False
+# create a loop to go through each value in numList
+for numValue in numList:
+    # create a loop to go through each value in newList
+    for newValue in newList:
+        # put a conditional to check if the values from both lists are equal. if so, there is a duplicate number and the computer must be informed.
+        if numValue == newValue:
+            duplicateFound = True
+    # Once the loop runs through all of newList, if there wasn't a duplicate then the value in numList is added to the newList.
+    if duplicateFound == False:
+        newList.append(numValue)
+    # reset our variable that stores duplicates so that the updated lists can be checked again
+    duplicateFound = False
 print(newList)
 
-#I SOLVED IT!!!!!! This took so many tries but I finally figured out my issue in the code! I had to reset counter to 0 so that every number in newList was being checked to numList.
-    
+
+
+
+
+
+
+
+
+
+
+
+
+# while len(numList) > 0:
+#     while duplicateFound == False and counter < len(newList): #please replace while with for loop and insert coinfound condition inside loop in an if statement
+#         if numList[0] == newList[counter]:
+#             duplicateFound = True
+#         else:
+#             counter = counter + 1
+#     counter = 0
+#     if duplicateFound == False:
+#         newList.append(numList[0])
+#         numList.pop(0)
+#     else:
+#         numList.pop(0)
+#         duplicateFound = False
+# print(newList)
+
