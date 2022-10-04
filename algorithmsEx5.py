4dc4aa96e6da5251b5551e80abe1bcb9547d4d7b
Agam Cohen <agamlaugh@gmail.com>
2022-10-04 01:00:39 -0700

    documented code

--- algorithmsEx5.py
+++ algorithmsEx5.py
@@ -1,10 +1,14 @@
 numList = [3, 6, 7, 9, 1, 2, 0, 3, 5, 9, -1, 8]
 counter = 0
+# create a variable that stores the minimum value of the list
 min = numList[counter]
+# run loop until all of numList has been checked
 while counter < len(numList):
+    # check if the current minimum value is less than the value next to it. if not, the adjacent value becomes the new minimum
     if min > numList[counter]:
         min = numList[counter]
     counter = counter + 1
+# find index of minimum value
 index = numList.index(min)
 print(min, " - ", index)
 #hi
