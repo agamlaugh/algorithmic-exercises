3f5fa590b871540f4ba638232fe0171c55b14ae8
Agam Cohen <agamlaugh@gmail.com>
2022-10-04 00:34:25 -0700

    documented code

--- algorithmsEx1.py
+++ algorithmsEx1.py
@@ -1,11 +1,15 @@
+# create a function that asks what your name is, accepts input, and returns it
 def nameInput():
     print('What is your name?')
     name = input()
     return name
 
+#call the funtion and store it under a variable
 name = nameInput()
+# create a while loop so that unless the name is exit, the user is greeted with their name input.
 while name != 'exit':
     print('Welcome ', name)
+    # the loop continues to ask for another name until condition is met
     name = nameInput() 
 
 #works
