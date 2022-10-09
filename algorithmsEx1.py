ed93dfe541864849437f4461e4dfde66821e7b02
Agam Cohen <agamlaugh@gmail.com>
2022-10-09 01:42:53 -0700

    I am having trouble outputing this to another file. There are mistakes I don't understand

--- algorithmsEx1.py
+++ algorithmsEx1.py
@@ -4,12 +4,22 @@ def nameInput():
     name = input()
     return name
 
+# create a function to change output if name is exit
+def exitChecker(name):
+    # create a while loop so that unless the name is exit, 
+    # the user is greeted with their name input.
+    while name != 'exit':
+        print('Welcome ', name)
+        # the loop continues to ask for another name until condition is met
+        name = nameInput() 
+
 #call the funtion and store it under a variable
 name = nameInput()
-# create a while loop so that unless the name is exit, 
-# the user is greeted with their name input.
-while name != 'exit':
-    print('Welcome ', name)
-    # the loop continues to ask for another name until condition is met
-    name = nameInput() 
 
+# call the function
+exitChecker(name)
+
+# send input and output history to new file
+f = open("/Users/meytallitmanovitz/coding/algorithmic-exercises/algorithmsEx1outputs.txt", "a")
+f.write(f"{name} --> {exitChecker(name)}\n")
+f.close()
